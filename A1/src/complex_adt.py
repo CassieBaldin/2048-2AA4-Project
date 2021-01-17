395f3b830ecf82c7456721ff29de3fede438f220
Cassie <baldic1@mcmaster.ca>
2021-01-17 00:14:31 -0500

    Basic start (commit test)

--- A1/src/complex_adt.py
+++ A1/src/complex_adt.py
@@ -1,5 +1,18 @@
 ## @file complex_adt.py
-#  @author 
-#  @brief 
-#  @date 
+#  @author Cassidy Baldin 
+#  @brief Contains a class for creating a complex number
+#  @date January 21st, 2021
 
+import math
+
+## @brief An ADT for representing dates
+#  @details A complex number composed of a real value x, and an imaginary value y
+class ComplexT:
+
+    ## @brief Constructor for ComplexT
+    #  @details Creates a complex number given a real value x, and imaginary value y
+    #  @param x Float representing real value of complex number
+    #  @param y Float representing imaginary value of complex number
+    def __init__ (self, x, y):
+        self.x = x
+        self.y = y
