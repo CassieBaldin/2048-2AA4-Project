3c9c17284ec096bd14802e682fcbeaf5a371f425
Cassie <baldic1@mcmaster.ca>
2021-01-18 14:03:22 -0500

    First five methods added

--- A1/src/complex_adt.py
+++ A1/src/complex_adt.py
@@ -5,7 +5,7 @@
 
 import math
 
-## @brief An ADT for representing dates
+## @brief An ADT for complex numbers
 #  @details A complex number composed of a real value x, and an imaginary value y
 class ComplexT:
 
@@ -16,3 +16,37 @@ class ComplexT:
     def __init__ (self, x, y):
         self.x = x
         self.y = y
+
+    ## @brief Gets the real value of the complex number
+    #  @return Float value representing real number
+    def real(self)
+    	return self.x
+
+    ## @brief Gets the imaginary number of the complex number
+    #  @return Float value representing imaginary number
+    def imag(self):
+    	return self.y
+
+    ## @brief Gets the absolute value (or modulus/magnitude) of the complex number
+    #  @return Float representing absolute value of the complex number
+    def get_r(self):
+    	return math.sqrt(self.x**2 + self.y**2)
+
+    ## @brief Gets the argument (or phase) of the complex number in radians
+    #  @return Float representing the phase of the complex number in radians
+    def get_phi(self):
+        if (self.x < 0 and self.y == 0):
+            return math.pi
+        try:
+            return 2*(math.atan(self.y/((math.sqrt(self.x**2 + self.y**2)) + self.x)))
+        except ZeroDivisionError:
+            return "undefined"
+
+    ## @brief Checks if argument and current object are equal
+    #  @return True if the argument and the current object are equal
+    def equal(self, e):
+        if (e.x == self.x) and (e.y == self.y):
+            return True
+        else:
+            return False
+            
