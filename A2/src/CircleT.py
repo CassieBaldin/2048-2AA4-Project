fdeec541825dbae8c6bbc15cd8351f80efb4dec1
Cassie <baldic1@mcmaster.ca>
2021-02-09 22:29:33 -0500

    Fixed code to match flake8

--- A2/src/CircleT.py
+++ A2/src/CircleT.py
@@ -8,36 +8,35 @@ from Shape import Shape
 
 
 ##  @brief CircleT is used as a constructor for a circle
-class CircleT():
+class CircleT(Shape):
     ## @brief constructor for method CircleT
-    #  @details 
+    #  @details
     #  @param xs value representing the x value of the center of mass
     #  @param ys value representing the y value of the center of mass
     #  @param rs value representing the radius of the circle
     #  @param ms value representing mass of circle
-    #  @return value representing 
     def __init__(self, xs, ys, rs, ms):
-    	self.x = xs
-    	self.y = ys
-    	self.r = rs
-    	self.m = ms
+        self.x = xs
+        self.y = ys
+        self.r = rs
+        self.m = ms
 
     ## @brief cm_x returns the x value of the center of mass
     #  @return value representing the center of mass of the x value
     def cm_x(self):
-    	return self.x
+        return self.x
 
     ## @brief cm_y returns the y value of the center of mass
     #  @return value representing the center of mass of the y value
-   	def cm_y(self):
-   		return self.y
+    def cm_y(self):
+        return self.y
 
-   	## @brief mass returns the mass of the object
+    ## @brief mass returns the mass of the object
     #  @return value representing the mass of the object
-   	def mass(self):
-   		return self.m
+    def mass(self):
+        return self.m
 
-   	## @brief mass returns the inertia of the object
+    ## @brief mass returns the inertia of the object
     #  @return value representing the inertia of the object
-   	def m_inert(self):
-   		return (self.m * self.r**2)/2
+    def m_inert(self):
+        return (self.m * self.r**2) / 2
