097dfbd52522d0e982f6c8186df498ee627e0ecc
Cassie <baldic1@mcmaster.ca>
2021-02-13 23:36:13 -0500

    Added error; changed function names

--- A2/src/BodyT.py
+++ A2/src/BodyT.py
@@ -1,12 +1,12 @@
 ## @file BodyT.py
 #  @author Cassidy Baldin
-#  @brief Contains a module for a body
+#  @brief Contains a module for an unspecified body in space
 #  @date February 12th, 2021
 
 from Shape import Shape
 
 
-##  @brief BodyT is used as a constructor for a body of unknown shape
+##  @brief BodyT is used as a constructor for a body of unknown shape in space
 class BodyT(Shape):
     ## @brief constructor for method BodyT
     #  @details Assumes that arguments provided to the access programs
@@ -14,57 +14,61 @@ class BodyT(Shape):
     #  @param xs value representing the x value of the center of mass
     #  @param ys value representing the y value of the center of mass
     #  @param ms value representing mass of the object
-    #  @throws ValueError if
+    #  @throws ValueError if the length of all input sequences are not equal
+    #  @throws ValueError if values in sequence ms are less than zero
     def __init__(self, xs, ys, ms):
         if not (len(xs) == len(ys) == len(ms)):
             raise ValueError
+        for i in range (0, len(ms)):
+            if not (ms[i] > 0):
+                raise ValueError
 
-        self.cmx = self.__cm(xs, ms)
-        self.cmy = self.__cm(ys, ms)
-        self.m = self.__sum(ms)
-        self.moment = self.__mmom(xs, ys, ms) \
-            - self.__sum(ms) * (self.__cm(xs, ms)**2 + self.__cm(ys, ms)**2)
+        self.__cmx = self.__cm__(xs, ms)
+        self.__cmy = self.__cm__(ys, ms)
+        self.__m = self.__sum__(ms)
+        self.__moment = self.__mmom__(xs, ys, ms) \
+            - self.__sum__(ms) * (self.__cm__(xs, ms)**2 + self.__cm__(ys, ms)**2)
 
     ## @brief cm_x returns the x value of the center of mass
     #  @return value representing the center of mass of the x value
     def cm_x(self):
-        return self.cmx
+        return self.__cmx
 
     ## @brief cm_y returns the y value of the center of mass
     #  @return value representing the center of mass of the y value
     def cm_y(self):
-        return self.cmy
+        return self.__cmy
 
     ## @brief mass returns the mass of the object
     #  @return value representing the mass of the object
     def mass(self):
-        return self.m
+        return self.__m
 
     ## @brief m_inert returns the moment of inertia of the object
     #  @return value representing the moment inertia of the object
     def m_inert(self):
-        return self.moment
+        return self.__moment
 
     ## @brief sum returns the sum of a sequence of numbers
     #  @return value representing the sum of the input sequence
-    def __sum(self, ms):
+    def __sum__(self, ms):
         sum = 0
-        for u in range(len(ms)):
+        for u in range(0, len(ms)):
             sum = sum + ms[u]
         return sum
 
     ## @brief cm returns the center of mass of the object?
     #  @return value representing the center of mass of the object
-    def __cm(self, z, m):
+    def __cm__(self, z, m):
         cm = 0
-        for i in range(len(m)):
-            cm = cm + (z[i] * m[i]) / sum(m)
+        for i in range(0, len(m)):
+            cm = cm + (z[i] * m[i]) / __sum__(m)
         return cm
 
-    ## @brief 
-    #  @return 
-    def __mmom(self, x, y, m):
+    ## @brief mmom returns the value of the moment of inertia of the body
+    #  @return value of the moment of inertia of the body
+    def __mmom__(self, x, y, m):
         mmom = 0
-        for i in range(len(m)):
+        for i in range(0, len(m)):
             mmom = mmom + m[i] * (x[i]**2 + y[i]**2)
         return mmom
