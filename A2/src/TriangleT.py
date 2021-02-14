85b9ec2efe31b1abf7b787cc5c5566ed740b4e48
Cassie <baldic1@mcmaster.ca>
2021-02-13 23:35:36 -0500

    Changed variable names

--- A2/src/TriangleT.py
+++ A2/src/TriangleT.py
@@ -7,7 +7,7 @@ from Shape import Shape
 
 
 ##  @brief TriangleT is used as a constructor for a triangle
-class BodyT(Shape):
+class TriangleT(Shape):
     ## @brief constructor for method BodyT
     #  @details Assumes that arguments provided to the access programs
     #           will be of the correct type
@@ -17,30 +17,30 @@ class BodyT(Shape):
     #  @param ms value representing mass of the triangle
     #  @throws ValueError if either side length or mass are less than zero
     def __init__(self, xs, ys, ss, ms):
-        self.x = xs
-        self.y = ys
-        self.s = ss
-        self.m = ms
+        self.__x = xs
+        self.__y = ys
+        self.__s = ss
+        self.__m = ms
 
-        if not ((self.s > 0) and (self.m > 0)):
+        if not ((self.__s > 0) and (self.__m > 0)):
             raise ValueError
 
     ## @brief cm_x returns the x value of the center of mass
     #  @return value representing the center of mass of the x value
     def cm_x(self):
-        return self.x
+        return self.__x
 
     ## @brief cm_y returns the y value of the center of mass
     #  @return value representing the center of mass of the y value
     def cm_y(self):
-        return self.y
+        return self.__y
 
     ## @brief mass returns the mass of the object
     #  @return value representing the mass of the object
     def mass(self):
-        return self.m
+        return self.__m
 
     ## @brief m_inert returns the moment of inertia of the object
     #  @return value representing the moment of inertia of the object
     def m_inert(self):
-        return (self.m * self.s**2) / 12
+        return (self.__m * self.__s**2) / 12
