55dc4d59d541b30dda702299b1a57c144098a2d9
Cassie <baldic1@mcmaster.ca>
2021-02-16 01:58:25 -0500

    Added basic CircleT test methods

--- A2/src/test_driver.py
+++ A2/src/test_driver.py
@@ -1,5 +1,29 @@
 ## @file test_All.py
 #  @author Cassidy Baldin
-#  @brief 
+#  @brief A module for testing modules CircleT, TriangleT, BodyT, and Scene.py
 #  @date February 12th, 2021
-#  @details
+
+from CircleT import CircleT
+from TriangleT import TriangleT
+from BodyT import BodyT
+from Scene import Scene
+
+from pytest import *
+
+
+class TestCircleT:
+    def test_cm_x(self):
+        c = CircleT(1.0, 10.0, 0.5, 5.0)
+        assert c.cm_x() == 1.0
+
+    def test_cm_y(self):
+        c = CircleT(1.0, 10.0, 0.5, 5.0)
+        assert c.cm_y() == 10.0
+
+    def test_mass(self):
+        c = CircleT(1.0, 10.0, 0.5, 5.0)
+        assert c.mass() == 5.0
+
+    def test_m_inert(self):
+        c = CircleT(1.0, 10.0, 0.5, 5.0)
+        assert c.m_inert() == 0.625
