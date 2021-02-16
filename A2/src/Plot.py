3da38ab5fd3ff1858c5496c6f84399b2ad83c649
Cassie <baldic1@mcmaster.ca>
2021-02-15 23:59:27 -0500

    Added new file

--- A2/src/Plot.py
+++ A2/src/Plot.py
@@ -6,21 +6,20 @@
 from matplotlib.pyplot import *
 
 def Plot(w, t):
-    ## @brief does somethin **FIX
+    ## @brief does something **FIX
     #  @details Assumes that the sequence will be built in order of increasing i values.
-    
+
     if not (len(w) == len(t)):
         raise ValueError
 
-    for i in range(len(w)):
-        x = w[i][0]
-        y = w[i][1]
+    fig, (ax1, ax2, ax3) = subplots(3, sharex=True)
+    fig.suptitle("Motion Simulation")
+    ax1.plot(w[0], t)
+    ax2.plot(w[1], t)
+    ax3.plot(w[1], w[0])
 
-        figure(dpi=100)
-        plot(x, t)
-        figure(dpi=100)
-        plot(y, t)
-        figure(dpi=100)
-        plot(y, x)
+    #     tight_layout()  ##to space the axis labels out more if you need to
 
     show()
+
+#print(Plot([[0, 2, 3, 4],[2, 4, 6, 8]], [1, 2]))
---
+++ A2/src/test_driver.py
@@ -0,0 +1,5 @@
+## @file test_All.py
+#  @author Cassidy Baldin
+#  @brief 
+#  @date February 12th, 2021
+#  @details
