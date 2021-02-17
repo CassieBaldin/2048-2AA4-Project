e13ca78fd79032d57019ee6d1b2e0ad07a1ac4bf
Cassie <baldic1@mcmaster.ca>
2021-02-16 23:54:26 -0500

    Attempted to test sim method

--- A2/src/test_driver.py
+++ A2/src/test_driver.py
@@ -192,25 +192,22 @@ class TestBodyT:
 class TestSceneT:
     def setup_method(self, method):
         def Fx(t):
-            return 5 if t < 5 else 0
+            return 0
 
         def Fy(t):
-            return -g * m if t < 3 else g * m
+            return -9.81
 
         self.c = CircleT(1.0, 10.0, 0.5, 5.0)
+        self.c2 = CircleT(1.0, 10.0, 0.5, 1.0)
         self.t = TriangleT(1.0, 10.0, 0.5, 24.0)
         self.b = BodyT([1, -1, -1, 1], [1, 1, -1, -1], [10, 10, 10, 10])
         self.s1 = Scene(self.c, Fx, Fy, 0, 0)
         self.s2 = Scene(self.t, Fx, Fy, 10, -10)
-
-        # def test_get_unbal_forces(self):
-        #     assert self.s1.get_unbal_forces() == (Fx, Fy)
-
-        # def test_get_unbal_forces2(self):
-        #     assert self.s2.get_unbal_forces() == (Fx, Fy)
+        self.s3 = Scene(self.c2, Fx, Fy, 0, 0)
 
     def teardown_method(self, method):
         self.c = None
+        self.c2 = None
         self.t = None
         self.s1 = None
         self.s2 = None
@@ -258,3 +255,63 @@ class TestSceneT:
     def test_undo_init_velo2(self):
         self.s2.set_init_velo(10, -10)
         assert self.s2.get_init_velo() == (10, -10)
+
+    def test_sim_t(self):
+        t, wsol = self.s3.sim(10, 10)
+        exp_t = [0.0, 1.111, 2.222, 3.333, 4.444, 5.555, 6.666, 7.777, 8.888, 10.0]
+
+        diff_t = []
+        for i in range(0, len(t)):
+            diff_t.append(t[i] - exp_t[i])
+
+        norm_t = 0
+        for i in range(0, len(diff_t)):
+            if abs(i) > norm_t:
+                norm_t = i
+
+        norm_exp = 0
+        for i in range(0, len(exp_t)):
+            if abs(i) > norm_exp:
+                norm_exp = i
+
+        assert (norm_t / norm_exp) == approx(1, rel=1e-3)
+
+    #I couldn't figure out how to effectively test this method :(
+    # def test_sim_wsol(self):
+    #     t, wsol = self.s3.sim(10, 10)
+    #     exp_wsol = [[1, 10, 0, 0],
+    #                 [1, 3.94444444, 0, -10],
+    #                 [1, -14.22222222, 0, -21.8],
+    #                 [1, -44.5, 0, -32.7],
+    #                 [1, -86.88888889, 0, -43.6],
+    #                 [1, -141.38888889, 0, -54.5],
+    #                 [1, -208, 0, -65.4],
+    #                 [1, -286.72222222, 0, -76.3],
+    #                 [1, -377.55555556, 0, -87.2],
+    #                 [1, -480.5, 0, -98.1]]
+
+    #     wsol_all = []
+    #     for i in wsol:
+    #         for j in range(len(i)):
+    #             wsol_all.append(i[j])
+
+    #     exp_all = []
+    #     for i in exp_wsol:
+    #         for j in range(len(i)):
+    #             exp_all.append(i[j])
+
+    #     diff_wsol = []
+    #     for i in wsol_all:
+    #         diff_wsol.append(wsol_all[i] - exp_all[i])
+
+    #     norm_wsol = 0
+    #     for i in range(0, len(diff_wsol)):
+    #         if abs(i) > norm_wsol:
+    #             norm_wsol = i
+
+    #     norm_exp = 0
+    #     for i in range(0, len(exp_all)):
+    #         if abs(i) > norm_exp:
+    #             norm_exp = i
+
+    #     assert (norm_wsol / norm_exp) == approx(1, rel=1e-3)
