e932cae69bbbcb4e580a30f0adfc1d9c2bca854f
Cassie <baldic1@mcmaster.ca>
2021-03-04 21:25:39 -0500

    All but union, diff, equals

--- Midterm/src/SetOfInt.py
+++ Midterm/src/SetOfInt.py
@@ -1,4 +1,43 @@
 ## @file SetOfInt.py
-#  @author Your Name
+#  @author Cassidy Baldin
 #  @brief Set of integers
 #  @date 03/04/2021
+
+class SetOfInt:
+
+    def __init__(self, xs):
+        #self.s is the union of all values in the sequence xs made into a set of ints
+        self.s = set(xs)
+
+    def is_member(self, x):
+        for i in self.s:
+            if x == i:
+                return True
+        return False
+    
+    def to_seq(self):
+        return self.__set_to_seq__(self.s)
+    
+    def union(self, t):
+        #retuns a new object of the class that is a union of the two sets of ints
+        new = self.__set_to_seq__(self.s)
+        for i in range(len(t.to_seq())):
+            new.append(t[i])
+
+        #where new is the new sequence of ints including all values from s and t
+        return SetOfInt(new)
+
+    def size(self):
+        return len(s)
+
+    def empty(self):
+        return self.s == []
+
+    def equals(self, t):
+        return False
+
+    def __set_to_seq__(self, s):
+        return list(s)
+
+    def __complement__(self, a):
+        new = {}
