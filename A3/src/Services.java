52c06941516c751b21e500de20f6ed310d65325c
Cassie <baldic1@mcmaster.ca>
2021-03-15 23:40:03 -0400

    Added method and local function

--- A3/src/Services.java
+++ A3/src/Services.java
@@ -1,1 +1,24 @@
 package src;
+
+public static Services
+{
+	public double[] normal(double[] v)
+	{
+		double[] n = [v.length]
+		for (double i : v) 
+		{
+			n[i] = i/sum(v);
+		}
+		return n;
+	}
+}
+
+public double sum(double[] x)
+{
+	total = 0;
+	for (double i : x)
+	{
+		total = total + i;
+	}
+	return total;
+}
