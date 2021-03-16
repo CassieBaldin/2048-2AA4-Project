31967c58acfcddf17c07d4e7bc08fa170bd38948
Cassie <baldic1@mcmaster.ca>
2021-03-15 22:35:24 -0400

    Added class and methods

--- A3/src/AttributeT.java
+++ A3/src/AttributeT.java
@@ -1,2 +1,31 @@
 package src;
 
+import java.util.*;
+
+public class AttributeT 
+{
+    private String name;
+    private T[] s;
+
+    public AttributeT(String attribName, T[] indicators)
+    {
+        name = attribName;
+        s = indicators;
+    }
+
+
+    public String getName() 
+    {
+        return name;
+    }
+
+    public T[] getIndicators() 
+    {
+        //returns list of all indicators 
+        return indicators;
+    }
+
+}
+
+
+
