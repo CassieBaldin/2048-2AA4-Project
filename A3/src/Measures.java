9b9872004a1245dd32d00ce2c0258ff17ff42c9a
Cassie <baldic1@mcmaster.ca>
2021-03-15 22:37:43 -0400

    Added interface and methods

--- A3/src/Measures.java
+++ A3/src/Measures.java
@@ -1,1 +1,21 @@
 package src;
+
+//returns [below expectations, marginal, meets expectations, exceeds expectations]
+
+interface Measures 
+{
+    public double[] measures() throws UnsupportedOperationException
+    {
+        throw new UnsupportedOperationException;
+    }
+
+    public double[] measures(IndicatorT ind) throws UnsupportedOperationException 
+    {
+        throw new UnsupportedOperationException;
+    }
+
+    public double[] measures(AttributeT att) throws UnsupportedOperationException 
+    {
+        throw new UnsupportedOperationException;
+    }
+}
