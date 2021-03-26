2a799c537ff13e415cfcdb415176489047f98f5d
Cassie <baldic1@mcmaster.ca>
2021-03-25 23:54:27 -0400

    Stuck on HashMap implementation for constructor :(

--- A3/src/CourseT.java
+++ A3/src/CourseT.java
@@ -1,1 +1,49 @@
 package src;
+
+import java.util.*;
+
+public class CourseT<IndicatorT, AttributeT, Measures, Services, Norm, LOsT>
+{
+    private String name;
+    private HashMap<IndicatorT, Set<LOsT>> m;  ///FIX???///
+
+    public CourseT(String courseName, IndicatorT[] indicators)
+    {
+        name = courseName;
+        for (int i = 0; i < indicators.length; i++)
+        {
+            m.put(indicators[i]);
+        }
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    // public IndicatorT[] getIndicators()
+    // {
+    //     System.out.println(m); //PROBABLY NEED TO CHANGE
+    //     return new IndicatorT[]
+    // }
+
+    // public LOsT[] getLOs(IndicatorT indicator)
+    // {
+
+    // }
+
+    // public void addLO(IndicatorT indicator, LOsT outcome)
+    // {
+
+    // }
+
+    // public void delLO(IndicatorT indicator, LOst outcome)
+    // {
+
+    // }
+
+    // public boolean member(IndicatorT indicator, LOsT outcomes)
+    // {
+
+    // }
+}
