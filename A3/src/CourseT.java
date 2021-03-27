cbee9abb86c890013d0ca80678ebacf14f0a2a0f
Cassie <baldic1@mcmaster.ca>
2021-03-27 18:27:53 -0400

    Fixed indicator implementation, added first 3 methods

--- A3/src/CourseT.java
+++ A3/src/CourseT.java
@@ -2,17 +2,19 @@ package src;
 
 import java.util.*;
 
-public class CourseT<IndicatorT, AttributeT, Measures, Services, Norm, LOsT>
+public class CourseT
 {
     private String name;
-    private HashMap<IndicatorT, Set<LOsT>> m;  ///FIX???///
+    private Map<IndicatorT, Set<LOsT>> m = new HashMap<IndicatorT, Set<LOsT>>();  ///FIX???///
 
     public CourseT(String courseName, IndicatorT[] indicators)
     {
         name = courseName;
         for (int i = 0; i < indicators.length; i++)
         {
-            m.put(indicators[i]);
+            IndicatorT ind = indicators[i];
+            Set<LOsT> lost = new HashSet<LOsT>();
+            m.put(ind, lost);
         }
     }
 
@@ -21,29 +23,42 @@ public class CourseT<IndicatorT, AttributeT, Measures, Services, Norm, LOsT>
         return name;
     }
 
-    // public IndicatorT[] getIndicators()
-    // {
-    //     System.out.println(m); //PROBABLY NEED TO CHANGE
-    //     return new IndicatorT[]
-    // }
+    public IndicatorT[] getIndicators()
+    {
+        IndicatorT[] ind = new IndicatorT[m.size()];
+        int index = 0;
+        for (IndicatorT i : m.keySet())
+        {
+            ind[index++] = i;
+        }
+        return ind;
+    }
 
-    // public LOsT[] getLOs(IndicatorT indicator)
-    // {
+    public LOsT[] getLOs(IndicatorT indicator)
+    {
+        LOsT[] lost = new LOsT[m.size()];
+        int index = 0;
+        boolean match = false;
+        for (IndicatorT i : m.keySet())
+        {
+            if (i == indicator)
+            {
+                match = true;
+                lost = set_to_seq(m.get(i));
+            } 
+        }
+        if (!match) {return lost;}
+        return lost;
+    }
 
-    // }
-
-    // public void addLO(IndicatorT indicator, LOsT outcome)
-    // {
-
-    // }
-
-    // public void delLO(IndicatorT indicator, LOst outcome)
-    // {
-
-    // }
-
-    // public boolean member(IndicatorT indicator, LOsT outcomes)
-    // {
-
-    // }
+    private LOsT[] set_to_seq(Set<LOsT> s)
+    {
+        LOsT lost[] = new LOsT[s.size()];
+        int index = 0;
+        for (LOsT i : s)
+        {
+            lost[index++] = i;
+        }
+        return lost;
+    }
 }
