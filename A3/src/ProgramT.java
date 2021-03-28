5879b821c617720315fa5cce283f2a5ef05b5dd4
Cassie <baldic1@mcmaster.ca>
2021-03-27 23:54:11 -0400

    Basic start to measures methods

--- A3/src/ProgramT.java
+++ A3/src/ProgramT.java
@@ -1,1 +1,23 @@
 package src;
+
+import java.util.*;
+
+public class ProgramT extends HashSet<CourseT> implements Measures 
+{
+
+    // public double[] measures() 
+    // {
+    //     throw new UnsupportedOperationException(); 
+    // }
+
+    // public double[] measures(IndicatorT ind) 
+    // {
+    //     throw new UnsupportedOperationException();
+    // }
+
+    // public double[] measures(Attribute att) 
+    // {
+    //     throw new UnsupportedOperationException();
+    // }
+
+}
