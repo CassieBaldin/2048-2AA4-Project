f9a15a1805b6d8a65d45e26065557a4403d3cf3c
Cassie <baldic1@mcmaster.ca>
2021-03-27 23:43:51 -0400

    Changed measures to include Norm, fized other measures methods

--- A3/src/LOsT.java
+++ A3/src/LOsT.java
@@ -1,13 +1,15 @@
 package src;
 
-public class LOsT<IndicatorT, AttributeT, Measures, Services, Norm>
+import java.util.*;
+
+public class LOsT<Measures>
 {
     private String name;
     private int n_blw;
     private int n_mrg;
     private int n_mts;
     private int n_exc;
-
+ 
     public LOsT(String topic, int nblw, int nmrg, int nmts, int nexc)
     {
         if (nblw < 0 || nmrg < 0 || nmts < 0 || nexc < 0) 
@@ -38,30 +40,26 @@ public class LOsT<IndicatorT, AttributeT, Measures, Services, Norm>
 
     public double[] measures()
     {
-        /////FIX TO MATCH OUT IN SPEC/////
         double[] measure = {n_blw, n_mrg, n_mts, n_exc};
-        return measure;
 
-        // Norm n = new Norm()
-        // if (!n.getNLOs())
-        // {
-        //     return measure;
-        // }
-        // else
-        // {
-        //     return (Services.normal(measure))
-        // }
+        Norm.setNorms(false, false, false);  //will always be true, might need to change?
+        if (!Norm.getNLOs())
+        {
+            return measure;
+        }
+        else
+        {
+            return (Services.normal(measure));
+        }
     }
 
-    /////FIX/////
     public double[] measures(IndicatorT ind) 
     {
         throw new UnsupportedOperationException();
     }
 
-    /////FIX/////
-    // public double[] measures(AttributeT att) 
-    // {
-    //     throw new UnsupportedOperationException();
-    // }
+    public double[] measures(AttributeT att) 
+    {
+        throw new UnsupportedOperationException();
+    }
 }
