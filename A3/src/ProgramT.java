1d82bfc898ce500b25e4134a0d2bb40bcce51e8e
Cassie <baldic1@mcmaster.ca>
2021-03-28 22:55:01 -0400

    Added state variable, added measures(att) method

--- A3/src/ProgramT.java
+++ A3/src/ProgramT.java
@@ -2,22 +2,38 @@ package src;
 
 import java.util.*;
 
-public class ProgramT extends HashSet<CourseT> implements Measures 
+public class ProgramT extends HashSet<CourseT> //implements Measures??
 {
+    private Set<CourseT> s = new HashSet<CourseT>();
 
-    // public double[] measures() 
-    // {
-    //     throw new UnsupportedOperationException(); 
-    // }
+    public double[] measures() 
+    {
+        throw new UnsupportedOperationException(); 
+    }
 
-    // public double[] measures(IndicatorT ind) 
-    // {
-    //     throw new UnsupportedOperationException();
-    // }
+    public double[] measures(IndicatorT ind) 
+    {
+        throw new UnsupportedOperationException();
+    }
 
-    // public double[] measures(Attribute att) 
-    // {
-    //     throw new UnsupportedOperationException();
-    // }
+    public double[] measures(Attribute att) 
+    {
+        double n[] = {0, 0, 0, 0};
+        for (CourseT c : s)
+        {
+            n = sumMeas(n, c.measures(att));
+        }
+        return Services.normal(n);
+    }
 
+    private double[] sumMeas(double[] seq1, double[] seq2)
+    {
+        //assume the sequences are the same length (4)
+        double sum[] = new double[seq1.length];
+        for(int i = 0; i < seq1.length; i++)
+        {
+            sum[i] = seq1[i] + seq2[i];
+        }
+        return sum;
+    }
 }
