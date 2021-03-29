9ea63018126162d7239693b2ed1372fd70a77c4f
Cassie <baldic1@mcmaster.ca>
2021-03-28 23:39:56 -0400

    Added indicator tests, some boundary tests

--- A3/src/TestAttributeT.java
+++ A3/src/TestAttributeT.java
@@ -2,7 +2,7 @@
  * Author: Cassidy Baldin
  * Revised: March 29th, 2021
  * 
- * Description: Testing module for AttributeT class and methods
+ * Description: Testing module for AttributeT class methods
  */
 
 package src;
@@ -16,9 +16,9 @@ public class TestAttributeT
 {
     private AttributeT Test1;
     private AttributeT Test2;
-    private AttributeT MultipleInd;
     private AttributeT Space;
-    private AttributeT NoName;
+    private AttributeT EmptyName;
+    private AttributeT MultipleInd;
     private AttributeT NoInd;
 
     @Before
@@ -26,11 +26,11 @@ public class TestAttributeT
     {
         Test1 = new AttributeT("Test1", new IndicatorT[] {IndicatorT.math, IndicatorT.specEngKnow});
         Test2 = new AttributeT("Test2", new IndicatorT[] {IndicatorT.assumpt});
+        Space = new AttributeT(" ", new IndicatorT[] {IndicatorT.math, IndicatorT.openEnded});
+        EmptyName = new AttributeT("", new IndicatorT[] {IndicatorT.math, IndicatorT.specEngKnow});
         MultipleInd = new AttributeT("MultipleInd", new IndicatorT[] {IndicatorT.desProcess, IndicatorT.desPrinciples,
                                                                      IndicatorT.openEnded, IndicatorT.ideaGeneration,
-                                                                     IndicatorT.healthSafety, IndicatorT.desProcess});
-        Space = new AttributeT(" ", new IndicatorT[] {IndicatorT.math, IndicatorT.openEnded});
-        NoName = new AttributeT("", new IndicatorT[] {IndicatorT.math, IndicatorT.specEngKnow});
+                                                                     IndicatorT.openEnded, IndicatorT.desProcess});
         NoInd = new AttributeT("NoInd", new IndicatorT[] {});
     }
 
@@ -39,9 +39,9 @@ public class TestAttributeT
     {
         Test1 = null;
         Test2 = null;
-        MultipleInd = null;
         Space = null;
-        NoName = null;
+        EmptyName = null;
+        MultipleInd = null;
         NoInd = null;
     }
 
@@ -58,14 +58,64 @@ public class TestAttributeT
     }
 
     @Test
+    public void test_chicken()
+    {
+        assertTrue(!(Test2.getName() == "Chicken :) "));
+    }
+
+    @Test
     public void test_space_name()
     {
         assertTrue(Space.getName() == " ");
     }
 
     @Test
-    public void test_noName_name()
+    public void test_emptyName_name()
     {
-        assertTrue(NoName.getName() == "");
+        assertTrue(EmptyName.getName() == "");
     }
+
+    @Test
+    public void test1_inds()
+    {
+        IndicatorT inds[] = new IndicatorT[] {IndicatorT.math, IndicatorT.specEngKnow};
+        HashSet<IndicatorT> set1 = new HashSet<IndicatorT>(Arrays.asList(inds));
+        HashSet<IndicatorT> set2 = new HashSet<IndicatorT>(Arrays.asList(Test1.getIndicators()));
+        assertTrue(set1.equals(set2));
+    }
+
+    @Test
+    public void test2_inds()
+    {
+        //only one input so do not need a hashset to check order
+        IndicatorT inds[] = new IndicatorT[] {IndicatorT.assumpt};
+        assertTrue(Arrays.equals(Test2.getIndicators(), inds));
+    }
+
+    @Test
+    public void test_mult_inds()
+    {
+        IndicatorT inds[] = new IndicatorT[] {IndicatorT.desProcess, IndicatorT.desPrinciples,
+                                              IndicatorT.openEnded, IndicatorT.ideaGeneration};
+        HashSet<IndicatorT> set1 = new HashSet<IndicatorT>(Arrays.asList(inds));
+        HashSet<IndicatorT> set2 = new HashSet<IndicatorT>(Arrays.asList(MultipleInd.getIndicators()));
+        assertTrue(set1.equals(set2));
+    }
+
+    @Test
+    public void test_no_ind()
+    {
+        IndicatorT inds[] = new IndicatorT[] {};
+        HashSet<IndicatorT> set1 = new HashSet<IndicatorT>(Arrays.asList(inds));
+        HashSet<IndicatorT> set2 = new HashSet<IndicatorT>(Arrays.asList(NoInd.getIndicators()));
+        assertTrue(set1.equals(set2));
+    }
+
+    @Test
+    public void test_not_ind()
+    {
+        IndicatorT inds[] = new IndicatorT[] {IndicatorT.assumpt, IndicatorT.math};
+        assertTrue(!Arrays.equals(Test2.getIndicators(), inds));
+    }
+
 }
