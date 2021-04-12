fc5e4a1208229024401b2119141413dde55fff1f
Cassie <baldic1@mcmaster.ca>
2021-04-11 23:28:18 -0400

    Added Play controller to help test some methods :)

---
+++ A4/src/Play.java
@@ -0,0 +1,58 @@
+package src;
+
+import java.util.*;
+
+public class Play
+{
+    private static boolean play_again = true;
+
+    public static void Play()
+    {
+        while (play_again) {
+            play_again = false;
+
+            Score.set_score(0);
+            int[][] board = new int[4][4];
+            Board b = new Board();
+            b.start();
+
+            System.out.println(" ");
+            Scanner user = new Scanner(System.in);
+            System.out.println("Controls: Move the tiles using wasd (up, left, down, right)");
+            System.out.println("Merge similar tiles to increase your score");
+            System.out.println("Join the tiles, get to 2048!");
+            b.view();
+            System.out.println("Which way would you like to move first? (w, a, s, d)");
+
+            String move = user.nextLine();
+            while (!game_over()) {
+                b.move(valid_move_check(move));
+                b.view();
+
+                System.out.println("Next move? (w, a, s, d)");
+                move = user.nextLine();
+            }
+
+            System.out.println("Game over :(");
+            System.out.println("Would you like to play again? (y/n)");
+            String ans = user.nextLine(); 
+            if ((ans.equals("y")) || (ans.equals("Y"))) {
+                play_again = true;
+            }
+        }
+    }
+
+    private static boolean game_over() {
+        //go through all the values, if all non-zero it is full, and if no possible merges, game over
+        return false;
+    }
+
+    private static String valid_move_check(String move) {
+        Scanner user = new Scanner(System.in);
+        if (!move.equals("w") && !move.equals("a") && !move.equals("s") && !move.equals("d")) {
+            System.out.println("Sorry that is not a valid move, please try again: (wasd)");
+            valid_move_check(user.nextLine());
+        }
+        return move;
+    }
+}
