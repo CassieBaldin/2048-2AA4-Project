bb2a211bb9f92b6323cbeedda4e613fe45e3c57e
Cassie <baldic1@mcmaster.ca>
2021-04-11 23:28:46 -0400

    Test stuff and Play controller test added

--- A4/src/A4Tests.java
+++ A4/src/A4Tests.java
@@ -6,5 +6,24 @@ public class A4Tests
 {
    public static void main(String[] args) {
 
-        System.out.println("Test");
+        // int[][] board = new int[4][4];
+
+        // Board b = new Board();
+        Play game = new Play();
+        game.Play();
+        // b.start(board);
+        // b.view(board);
+
+        // System.out.println(" ");
+        // System.out.println(Arrays.toString(b.get_row(0)));
+        // System.out.println(Arrays.toString(b.get_row(1)));
+        // System.out.println(Arrays.toString(b.get_row(2)));
+        // System.out.println(Arrays.toString(b.get_row(3)));
+    
+        // Scanner user = new Scanner(System.in);
+        // System.out.println("Type: ");
+        // String move = user.nextLine();
+        // String s = valid_move_check(move);
+        // System.out.println("Done");
+	}
 }
