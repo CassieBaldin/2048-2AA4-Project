f6d75645ac456584522f94fc894f199d500bd656
Cassie <baldic1@mcmaster.ca>
2021-04-12 21:25:26 -0400

    Added doxygen comments, fixed check_game_over and check_win methods

--- A4/src/Play.java
+++ A4/src/Play.java
@@ -1,3 +1,9 @@
+/**
+ * Author: Cassidy Baldin
+ * Revised: March 29th, 2021
+ * 
+ * Description: Controller method to play the game 2048!
+ */
 package src;
 
 import java.util.*;
@@ -6,19 +12,24 @@ public class Play
 {
     private static boolean play_again = true;
     private static boolean game_over = false;
+    private static Board b = new Board();
 
+    /**
+    * @brief Initializes game start by printing controls and making new Board object to play
+    * @details Assumes SetUp will be called on a new Board object. 
+    */
     public static void Play()
     {
         while (play_again) {
             play_again = false;
             game_over = false;
 
-            Score.set_score(0);
-            Board b = new Board();
+            Score.reset_score();
             b.start();
 
             System.out.println(" ");
             Scanner user = new Scanner(System.in);
+            System.out.println("Welcome to 2048!! :)");
             System.out.println("Controls: Move the tiles using wasd (up, left, down, right)");
             System.out.println("Merge similar tiles to increase your score");
             System.out.println("Join the tiles, get to 2048!");
@@ -26,7 +37,7 @@ public class Play
             System.out.println("Which way would you like to move first? (w, a, s, d)");
 
             String move = user.nextLine();
-            while (!check_game_over()) {
+            while (!game_over) {
                 b.move(valid_move_check(move));
                 b.view();
 
@@ -42,6 +53,7 @@ public class Play
                 else {
                     System.out.println("Next move? (w, a, s, d)");
                     move = user.nextLine();
+                    check_game_over();
                 }
             }
 
@@ -54,19 +66,52 @@ public class Play
         }
     }
 
+    /**
+    * @brief Goes through current game board and check if a move/merge can be made; if not game over
+    * @return Boolean representing if the game is over (all cells full and no merges available)
+    */
     private static boolean check_game_over() {
-        //go through all the values, if all non-zero it is full, and if no possible merges, game over
-        game_over = false;
+        int check = 0;
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                int value = b.get_board_value(i, j);
+                if (value == 0) {check++; }
+                if ((i-1 >= 0) && (b.get_board_value(i-1, j) == value)) {check++; } //check above cell
+                if ((i+1 <= 3) && (b.get_board_value(i+1, j) == value)) {check++; } //check below cell
+                if ((j-1 >= 0) && (b.get_board_value(i, j-1) == value)) {check++; } //check left cell
+                if ((j+1 <= 3) && (b.get_board_value(i, j+1) == value)) {check++; } //check right cell
+                
+                if (check > 0) {
+                    return game_over;
+                }
+            }
+        }
+        if (check == 0) { 
+            game_over = true;
+            return game_over;
+        }
         return game_over;
     }
 
+    /**
+    * @brief Checks if any tile in current board has value 2048, if so the user wins!!
+    * @return Boolean representing if the user won or not
+    */
     private static boolean check_win() {
-        if (Score.get_score() == 2048) {
-            return true;
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                if (b.get_board_value(i, j) == 2048) {
+                    return true;
+                }
+            }
         }
         else {return false; }
     }
 
+    /**
+    * @brief Checks if the direction the user input is valid, if not asks the user to try again
+    * @return String representing the accepted move that the user input
+    */
     private static String valid_move_check(String move) {
         while (!move.equals("w") && !move.equals("a") && !move.equals("s") && !move.equals("d")) {
             Scanner user = new Scanner(System.in);
