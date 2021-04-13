e6c1d57042706f02fdff735464e87a652a478a55
Cassie <baldic1@mcmaster.ca>
2021-04-12 21:13:46 -0400

    Added doxygen comments

--- A4/src/SetUp.java
+++ A4/src/SetUp.java
@@ -13,19 +13,19 @@ import java.util.*;
 * @brief This class represents the start state of the game
 * @details Two random tiles will be added in random spots on the 4x4 board. 
 */
-
 public class SetUp
 {
     private int[][] game_board = new int[4][4];
+
    /**
-    * @brief Initializes game start state
+    * @brief Initializes game start state by filling two random spots on the board with a random number (2 or 4)
+    * @details Assumes SetUp will be called on a new Board object. 
     */
     public SetUp()
     {
         int[] random_spot1 = random_entry();
         int[] random_spot2 = random_entry();
 
-        //make sure two spots are filled instead of 1
         while (Arrays.equals(random_spot1, random_spot2)) {
             random_spot2 = random_entry();
         }
@@ -34,6 +34,10 @@ public class SetUp
         game_board[random_spot2[0]][random_spot2[1]] = random_num();
     }
 
+    /**
+    * @brief A random number where 2 is 90% likely, and 4 is 10% likely.
+    * @return The number 2 or 4.
+    */
     private int random_num() {
         Random r = new Random();
         int x = r.nextInt(100);
@@ -42,6 +46,10 @@ public class SetUp
         else { return 2; }
     }
 
+    /**
+    * @brief A random entry (cell) on a 4x4 grid.
+    * @return A set of values representing coordinates on a 4x4 grid.
+    */
     private int[] random_entry() {
         Random r = new Random();
         int row = r.nextInt(4);
@@ -51,6 +59,10 @@ public class SetUp
         return index;
     }
 
+    /**
+    * @brief Gets the start state of the board.
+    * @return The game_board in its current start state. 
+    */
     public int[][] get_board() {
         return game_board;
     }
