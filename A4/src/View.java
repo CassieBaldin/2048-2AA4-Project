2cc3d7cb99584b03aa09f9f5a33abc0e3addd235
Cassie <baldic1@mcmaster.ca>
2021-04-12 21:14:47 -0400

    Added doxygen comments, changed print statements to loop single statement

--- A4/src/View.java
+++ A4/src/View.java
@@ -2,7 +2,7 @@
  * Author: Cassidy Baldin
  * Revised: April 12th, 2021
  * 
- * Description: View class to visualize the state of the game
+ * Description: View class to visualize the state of the board.
  */
 
 package src;
@@ -10,21 +10,21 @@ package src;
 import java.util.*;
 
 /**
-* @brief This class represents a way to view the current state of the input board
+* @brief This class represents a way to view the current state of the input board.
 */
 
 public class View
 {
    /**
-    * @brief Visiualizer for the game, prints board state to the screen
+    * @brief Visualizer for the game, prints board state to the screen.
+    * @param game_board Grid representing the current game board.
     */
     public View(int[][] game_board)
     {
         System.out.println(" ");
         System.out.println("Score: " + Score.get_score());
-        System.out.println(Arrays.toString(game_board[0]));
-        System.out.println(Arrays.toString(game_board[1]));
-        System.out.println(Arrays.toString(game_board[2]));
-        System.out.println(Arrays.toString(game_board[3]));
+        for (int i = 0; i < game_board[0].length; i++) {
+            System.out.println(Arrays.toString(game_board[i]));
+        }
     }
 }
