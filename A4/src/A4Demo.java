eaea66987185bb8d7d4e846c543bce73848c6688
Cassie <baldic1@mcmaster.ca>
2021-04-12 21:31:03 -0400

    Added demo to be able to play the game with controller

---
+++ A4/src/A4Demo.java
@@ -0,0 +1,20 @@
+/**
+ * Author: Cassidy Baldin
+ * Revised: MApril 12th, 2021
+ * 
+ * Description: Demo for play method to be able to play the game with the controller :)
+ */
+
+package src;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+import java.util.*;
+
+public class A4Tests
+{
+   public static void main(String[] args) {
+        Play game = new Play();
+        game.Play();
+    }
+}
