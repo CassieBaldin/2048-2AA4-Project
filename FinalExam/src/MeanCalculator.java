fca71860b97d7c27652560af0ae15e20eedbc9c5
Cassie <baldic1@mcmaster.ca>
2021-04-28 21:29:08 -0400

    made meanCalc an interface, changed imp

--- FinalExam/src/MeanCalculator.java
+++ FinalExam/src/MeanCalculator.java
@@ -1,15 +1,7 @@
 package src;
 import java.util.*;
 
-public class MeanCalculator
+public interface MeanCalculator
 {
-    public static Double meanCalc(ArrayList<Double> seq) {
-    	double mean = 0;
-
-    	for (double i : seq) {
-            mean = mean + i;
-        }
-        mean = mean/seq.size();
-    	return mean;
-    }
+    public Double meanCalc(ArrayList<Double> seq);
 }
